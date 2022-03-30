b09a1fd430137fa4580f74f6f1016626c97dce57
pankaj kumar <raipankaj150@gmail.com>
2022-03-30 13:44:48 +0530

    available meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,42 @@
+import { useEffect,useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css"
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
 
 const AvailableMeals = ()=>{
-    const mealsList = DUMMY_MEALS.map(meal=> <MealItem key={meal.id} id={meal.id}
+  const [meals,setMeals] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+
+    useEffect(()=>{
+      const fetchMeals = async ()=>{
+       const response = await fetch('https://react-practise-b6d2c-default-rtdb.firebaseio.com/meals.json');
+       const respenseData = await response.json();
+
+       const loadedMeals = [];
+       for(const key in respenseData){
+         loadedMeals.push({
+           id:key,
+           name:respenseData[key].name,
+           description:respenseData[key].description,
+           price:respenseData[key].price,
+         });
+       }
+       
+       setMeals(loadedMeals);
+       setIsLoading(false);
+      }
+      
+      fetchMeals();
+    },[])
+
+    if(isLoading){
+      return <section className={classes.mealsLoading}>
+        <p>Loading.....</p>
+      </section>
+    }
+
+    const mealsList = meals.map(meal=> <MealItem key={meal.id} id={meal.id}
       name={meal.name}
       price={meal.price}
       description={meal.description} />)
