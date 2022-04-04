60fe5dd8121943922e22c166f04ee8c06eda34bc
pankaj kumar <raipankaj150@gmail.com>
2022-04-04 08:30:51 +0530

    error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,10 +7,15 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = ()=>{
   const [meals,setMeals] = useState([]);
   const [isLoading,setIsLoading] = useState(true);
+  const [httpError,setHttpError] = useState();
 
     useEffect(()=>{
       const fetchMeals = async ()=>{
        const response = await fetch('https://react-practise-b6d2c-default-rtdb.firebaseio.com/meals.json');
+
+       if(!response.ok){
+         throw new Error('Something went wrong');
+       }
        const respenseData = await response.json();
 
        const loadedMeals = [];
@@ -26,8 +31,13 @@ const AvailableMeals = ()=>{
        setMeals(loadedMeals);
        setIsLoading(false);
       }
+
+        fetchMeals().catch(error=>{
+          setIsLoading(false);
+        setHttpError(error.message);
+        });
+     
       
-      fetchMeals();
     },[])
 
     if(isLoading){
@@ -35,7 +45,11 @@ const AvailableMeals = ()=>{
         <p>Loading.....</p>
       </section>
     }
-
+    if(httpError){
+      return <section className={classes.mealsError}>
+      <p>{httpError}</p>
+    </section>
+    }
     const mealsList = meals.map(meal=> <MealItem key={meal.id} id={meal.id}
       name={meal.name}
       price={meal.price}
